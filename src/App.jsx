23874d3cb3109886dd7167b56fcffbbc28125c91
Farida-Hamid <faridaaddala@gmail.com>
2022-09-17 11:46:54 +0200

    Adjust App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Library from './components/Library';
+import Categories from './components/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
