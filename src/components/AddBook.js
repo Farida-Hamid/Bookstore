61656f44421529eaefcd118c74efa09c55b5ab59
Farida-Hamid <faridaaddala@gmail.com>
2022-09-20 16:14:58 +0200

    Connect the add book component to redux

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from '../redux/books/books';
 
-const AddBook = () => (
-  <form>
-    <h2>Add a book</h2>
-    <input type="text" placeholder="Title" name="title" />
-    <input type="text" placeholder="Author" name="author" />
-    <button type="submit">Add</button>
-  </form>
-);
+const AddBook = () => {
+  const bookList = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [state, setState] = useState({
+    title: '',
+    author: '',
+  });
 
+  return (
+    <form onSubmit={submit}>
+      <h2>Add a book</h2>
+      <input type="text" placeholder="Title" name="title" value={state.title} onChange={read} />
+      <input type="text" placeholder="Author" name="author" value={state.author} onChange={read} />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
 export default AddBook;
