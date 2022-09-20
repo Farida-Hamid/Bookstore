f15b76de2b585d3768930afc7fdac929260f454b
Farida-Hamid <faridaaddala@gmail.com>
2022-09-20 16:20:46 +0200

    Connect the categories component to redux

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,20 @@
-const Categories = () => <button type="button">Check status</button>;
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { check } from '../redux/categories/categories';
+
+const Categories = ({ categories }) => {
+  const dispatch = useDispatch();
+
+  const showStatus = () => {
+    dispatch(check());
+  };
+  return (
+    <>
+      <button type="button" onClick={() => showStatus()}>Check status</button>
+      <br />
+      {categories}
+    </>
+  );
+};
 
 export default Categories;
