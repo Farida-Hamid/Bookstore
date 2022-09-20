fe8c82779bd8d4fb0e55b4e0ba487c5ce4524e73
Farida-Hamid <faridaaddala@gmail.com>
2022-09-20 16:56:12 +0200

    Connect the library component to API

--- src/components/Library.js
+++ src/components/Library.js
@@ -9,11 +9,15 @@ const Library = (props) => {
   return (
     <div>
       {bookList.map((book) => (
-        <Book key={book.id} title={book.title} author={book.author} id={book.id} />
+        <Book key={book.item_id} title={book.title} author={book.author} id={book.item_id} />
       ))}
       <AddBook />
     </div>
   );
+};
+
+Library.defaultProps = {
+  books: [],
 };
 
 export default Library;
